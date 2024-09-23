a519838729f617f72bf27a580e64f17d773eca15
EuzoRodrigues <enzobarbaresco@gmail.com>
2024-09-23 20:30:53 -0300

    Commit

---
+++ Projeto/exibeTexto.php
@@ -0,0 +1,38 @@
+<?php 
+    $arquivo = fopen("texto.txt", "r");
+
+    $texto = "";
+
+    if($arquivo) {
+        $texto = fread($arquivo, 5000);
+    }
+    else {
+        $texto = "";
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> NOTÍCIAS </title>
+
+    <link rel="stylesheet" href="./css/estilo.css">
+
+    <?php include("compara.inc") ?>
+</head>
+<body>
+    <div class="title-container">
+        <div class="titulo">
+            <h3> BEM VINDO AO SITE WEBDESIGN EM FOCO </h3>
+        </div>
+    </div>
+    <div class="content-container language-text">
+        <div class="conteudo">
+            <p> <?php echo($texto) ?> </p>
+            <a href="/Projeto/reiniciaIdioma.php"> Selecionar outro idioma </a>
+        </div>
+    </div>
+</body>
+</html>
